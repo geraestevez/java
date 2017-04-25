72ffeb5306455ee5153c11106c105b2f224512d6
geraestevez <gerardo_271195@hotmail.com>
2017-04-25 15:00:18 -0500

    Add files via upload

---
+++ Inicio.js
@@ -0,0 +1,65 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/* global end */
+    $num=true;
+    $vIk=0;
+$("#iker").click(function(){
+    
+      if($num==true){
+   alert("Has votado a iker como mejor portero");
+    $vIk+=1;
+    $num=false;
+    end;
+    }
+  if($num==false){
+    alert("Ya has votado");
+    end;
+    }
+  });  
+$("#buffon").click(function(){
+    if($num==true){
+    alert("Has votado a Buffon como mejor portero");
+     $num=false;
+    end;
+    }
+    if($num==false){
+    alert("Ya has votado");
+    end;
+    }
+});
+$("#neuer").click(function(){
+    if($num==true){
+    alert("Has votado a Neuer como mejor portero");
+    $num=false;
+    end;
+    }
+    if($num==false){
+    alert("Ya has votado");
+    end;
+    }
+});
+$("#cech").click(function(){
+    if($num==true){
+    alert("Has votado a Cech como mejor portero");
+    $num=false;
+    end;
+    }
+    if($num==false){
+    alert("Ya has votado");
+    end;
+    }
+});
+$("#navas").click(function(){
+    if($num==true){
+    alert("Has votado a Keylor como mejor portero");
+    $num=false;
+    end;
+    }
+    if($num==false){
+    alert("Ya has votado");
+    end;
+    }
+});
