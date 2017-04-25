375a4f4e3f22b5b4f1e628729f3c70669ec9368d
geraestevez <gerardo_271195@hotmail.com>
2017-04-25 14:37:15 -0500

    Add files via upload

--- Inicio.js
+++ Inicio.js
@@ -3,10 +3,63 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
-$("#esta").click(function (){
-    alert("Me has apachurrado!!!");
+/* global end */
+    $num=true;
+    $vIk=0;
+$("#iker").click(function(){
     
-    
+      if($num==true){
+   alert("Has votado a iker como mejor portero");
+    $vIk+=1;
+    $num=false;
+    end;
+    }
+  if($num==false){
+    alert("Ya has votado");
+    end;
+    }
+  });  
+$("#buffon").click(function(){
+    if($num==true){
+    alert("Has votado a Buffon como mejor portero");
+     $num=false;
+    end;
+    }
+    if($num==false){
+    alert("Ya has votado");
+    end;
+    }
+});
+$("#neuer").click(function(){
+    if($num==true){
+    alert("Has votado a Neuer como mejor portero");
+    $num=false;
+    end;
+    }
+    if($num==false){
+    alert("Ya has votado");
+    end;
+    }
+});
+$("#cech").click(function(){
+    if($num==true){
+    alert("Has votado a Cech como mejor portero");
+    $num=false;
+    end;
+    }
+    if($num==false){
+    alert("Ya has votado");
+    end;
+    }
+});
+$("#navas").click(function(){
+    if($num==true){
+    alert("Has votado a Keylor como mejor portero");
+    $num=false;
+    end;
+    }
+    if($num==false){
+    alert("Ya has votado");
+    end;
+    }
 });
